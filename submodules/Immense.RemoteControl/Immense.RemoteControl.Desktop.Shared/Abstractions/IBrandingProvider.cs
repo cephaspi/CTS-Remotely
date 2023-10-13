f5b9de55c680d05cee95c420e900b4ea082bc0b9
CephasPi <peter.pisoja@conti.sk>
2023-10-13 09:14:32 +0200

    Add project files.

---
+++ submodules/Immense.RemoteControl/Immense.RemoteControl.Desktop.Shared/Abstractions/IBrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Immense.RemoteControl.Shared.Models;
+
+namespace Immense.RemoteControl.Desktop.Shared.Abstractions;
+
+public interface IBrandingProvider
+{
+    BrandingInfoBase CurrentBranding { get; }
+    Task Initialize();
+    void SetBrandingInfo(BrandingInfoBase brandingInfo);
+}
---
+++ submodules/Immense.RemoteControl/Immense.RemoteControl.Desktop.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Immense.RemoteControl.Desktop.Windows")]
+[assembly: InternalsVisibleTo("Immense.RemoteControl.Desktop.UI")]
+[assembly: InternalsVisibleTo("Immense.RemoteControl.Desktop.Linux")]
